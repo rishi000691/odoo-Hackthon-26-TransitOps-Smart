3aa87bc59fb93302a9ce866e9a16f93dd8903e04
Hari Raval <hariraval81@gmail.com>
2026-07-12 09:33:44 +0530

    chore: updated  .gitignore file

--- frontend/transitops/lib/main.dart
+++ frontend/transitops/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Transit Ops',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      // theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Transit Ops'),
     );
   }
